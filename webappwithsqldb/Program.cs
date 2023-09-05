36ced1a56e0d11e47de4cf4e1d5ddea9da5e14da
MADA, DILEEP (SHS TE DC IND DI-D&A DEV PLF NS1) <dileep.mada@siemens-healthineers.com>
2023-09-05 14:49:02 +0530

    second commit

--- webappwithsqldb/Program.cs
+++ webappwithsqldb/Program.cs
@@ -1,6 +1,15 @@
 using webappwithsqldb.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = "Endpoint=https://tempappconfig.azconfig.io;Id=hbNz;Secret=kJ+9HBAjhnZJIzVg20embttRnzLGwK1i7CZF8EXYoow=";
+//Install Microsoft.Extensions.Configuration.AzureAppConfiguration
+builder.Host.ConfigureAppConfiguration(builder =>
+{
+    //Connect to your App Config Store using the connection string
+    builder.AddAzureAppConfiguration(connectionString);
+});
+
 builder.Services.AddTransient<IProductService, ProductService>();
 
 // Add services to the container.
