1a4da1f13b49032e89d7f660204d2054b5e64262
MADA, DILEEP (SHS TE DC IND DI-D&A DEV PLF NS1) <dileep.mada@siemens-healthineers.com>
2023-09-05 19:47:11 +0530

    adding azure functions

---
+++ tempFunction/GetProduct.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tempFunction
+{
+    public static class GetProduct
+    {
+        [FunctionName("GetProduct")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Get data from the database");
+            List<Product> _product_lst = new List<Product>();
+            string _statement = "SELECT ProductID,ProductName,Quantity from Products";
+            SqlConnection _connection = GetConnection();
+
+            _connection.Open();
+
+            SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
+
+            using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
+            {
+                while (_reader.Read())
+                {
+                    Product _product = new Product()
+                    {
+                        ProductID = _reader.GetInt32(0),
+                        ProductName = _reader.GetString(1),
+                        Quantity = _reader.GetInt32(2)
+                    };
+
+                    _product_lst.Add(_product);
+                }
+            }
+            _connection.Close();
+            return new OkObjectResult(JsonConvert.SerializeObject(_product_lst));
+            //return new OkObjectResult(_product_lst);
+        }
+
+        private static SqlConnection GetConnection()
+        {
+            string connectionString = "Server=tcp:tempsqlserverdileep.database.windows.net;Initial Catalog=tempdatabase;user id=dileep;password=Temp31n@w;";
+            return new SqlConnection(connectionString);
+        }
+
+        //private static SqlConnection GetConnection()
+        //{
+        //    string connection_string = Environment.GetEnvironmentVariable("SQLAZURECONNSTR_SQLConnectionString");
+        //    return new SqlConnection(connection_string);
+        //}
+    }
+}
