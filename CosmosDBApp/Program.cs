fc88929de3501564a5e0c3c5d6a3e5ce708a5175
MADA, DILEEP (SHS TE DC IND DI-D&A DEV PLF NS1) <dileep.mada@siemens-healthineers.com>
2023-09-06 19:50:46 +0530

    adding other projects

---
+++ CosmosDBApp/Program.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Azure.Cosmos;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Xml.Linq;
+using Container = Microsoft.Azure.Cosmos.Container;
+
+namespace CosmosDBApp
+{
+    public class Program
+    {
+        static string cosmosDBEndpointUri = "https://dileepcosmosdb.documents.azure.com:443/";
+
+        static string cosmosDBKey =
+            "I7G7RksostOiPClHQzjKwV4jxBQeV5PajWD1eM5P4AmrbOe3uoTVPBeCpHsPgl6htm3vADConddsACDb5Z8iYw==";
+
+        static string databaseName = "appdb";
+        static string containerName = "Orders";
+
+        static void Main(string[] args)
+        {
+            // CreateDatabase(databaseName);
+            // CreateContainer(databaseName, containerName, "/category");
+            //AddItem("O1", "Laptop", 100);
+            //AddItem("O2", "Mobiles", 200);
+            //AddItem("O3", "Desktop", 75);
+            //AddItem("O4", "Laptop", 25);
+
+            CallStoredProcedure();
+            Console.ReadLine();
+        }
+
+        static void CreateDatabase(string databaseName)
+        {
+            CosmosClient cosmosClient;
+            cosmosClient = new CosmosClient(cosmosDBEndpointUri, cosmosDBKey);
+
+            cosmosClient.CreateDatabaseIfNotExistsAsync(databaseName).GetAwaiter().GetResult();
+            Console.WriteLine("Database created");
+        }
+
+        static void CreateContainer(string databaseName, string containerName, string partitionKey)
+        {
+            CosmosClient cosmosClient;
+            cosmosClient = new CosmosClient(cosmosDBEndpointUri, cosmosDBKey);
+
+            Database database = cosmosClient.GetDatabase(databaseName);
+
+            database.CreateContainerIfNotExistsAsync(containerName, partitionKey).GetAwaiter().GetResult();
+
+            Console.WriteLine("Container created");
+        }
+
+        static void AddItem(string orderId, string category, int quantity)
+        {
+            CosmosClient cosmosClient;
+            cosmosClient = new CosmosClient(cosmosDBEndpointUri, cosmosDBKey);
+
+            Database database = cosmosClient.GetDatabase(databaseName);
+            Container container = database.GetContainer(containerName);
+
+            Order order = new Order()
+            {
+                id = Guid.NewGuid().ToString(),
+                orderId = orderId,
+                category = category,
+                quantity = quantity
+            };
+
+            container.CreateItemAsync<Order>(order, new PartitionKey(order.category)).GetAwaiter().GetResult();
+
+            Console.WriteLine("Added item with Order Id {0}", orderId);
+            //Console.WriteLine("Request Units consumed {0}", response.RequestCharge);
+
+        }
+
+        static void CallStoredProcedure()
+        {
+            CosmosClient cosmosClient;
+            cosmosClient = new CosmosClient(cosmosDBEndpointUri, cosmosDBKey);
+            Database database = cosmosClient.GetDatabase(databaseName);
+            Container container = database.GetContainer(containerName);
+
+            dynamic[] orderItems = new dynamic[]
+            {
+                new
+                {
+                    id = Guid.NewGuid().ToString(),
+                    orderId = "01",
+                    category = "Laptop",
+                    quantity = 100
+                },
+                new
+                {
+                    id = Guid.NewGuid().ToString(),
+                    orderId = "02",
+                    category = "Laptop",
+                    quantity = 200
+                },
+                new
+                {
+                    id = Guid.NewGuid().ToString(),
+                    orderId = "03",
+                    category = "Laptop",
+                    quantity = 75
+                },
+            };
+            var result = container.Scripts.ExecuteStoredProcedureAsync<string>("createItems",
+                new PartitionKey("Laptop"), new[] { orderItems }).GetAwaiter().GetResult();
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
---
+++ DileepBlobApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+
+namespace DileepBlobApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=dileepstorageac;AccountKey=zihZrIwU9D9g1Qi5sBXgKVYOTaZdt55NoJjscVXQ4W8qWjDPMtgMzo1FeEU3nObJTth5sdA7JxR6+ASt6x6Npg==;EndpointSuffix=core.windows.net";
+            string containerName = "data1";
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            //Console.WriteLine("Creating the container");
+
+            //blobServiceClient.CreateBlobContainer(containerName);
+
+            /*
+            If you want to specify properties for the container
+
+           blobServiceClient.CreateBlobContainerAsync(containerName,Azure.Storage.Blobs.Models.PublicAccessType.Blob);
+            */
+            //Console.WriteLine("Container creation complete");
+
+            string blobName = "script.sql";
+            string filePath = "D:\\UserData\\z003f28n\\AzureLearning\\script.sql";
+            BlobContainerClient blobServiceClient1 = new BlobContainerClient(connectionString, containerName);
+
+            BlobClient blobClient = blobServiceClient1.GetBlobClient(blobName);
+            blobClient.UploadAsync(filePath, true);
+
+            //Console.WriteLine("Uploaded the blob");
+
+            Console.WriteLine("List the blobs");
+            BlobContainerClient blobContainerClient = new BlobContainerClient(connectionString, containerName);
+
+            foreach (BlobItem blobItem in blobContainerClient.GetBlobs())
+            {
+                Console.WriteLine("The Blob Name is {0}", blobItem.Name);
+                Console.WriteLine("The Blob Size is {0}", blobItem.Properties.ContentLength);
+            }
+
+            Console.WriteLine("Download blobs");
+            //BlobClient blobClient = new BlobClient(connectionString, containerName, blobName);
+
+            blobClient.DownloadToAsync(filePath);
+
+            Console.WriteLine("The blob is downloaded");
+
+
+            Console.ReadKey();
+        }
+    }
+}
